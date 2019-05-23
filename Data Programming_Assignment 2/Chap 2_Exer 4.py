a5ccd3c2fbd096dea277519df7a33963fbfb010c
Manish0195 <50720198+Manish0195@users.noreply.github.com>
2019-05-23 16:06:06 -0400

    Add files via upload

---
+++ Data Programming_Assignment 2/Chap 2_Exer 4.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+width = 17;
+height = 12.0;
+
+a = width//2;
+b = width/2.0;
+c = height/3;
+d = 1 + 2 * 5;
+
+print("width//2 = ", a ,"Type: ", type(a));
+print("width/2.0 = ", b ,"Type: ", type(b));
+print("height/3 = ", c ,"Type: ", type(c));
+print("1 + 2 * 5 = ", d ,"Type: ", type(d));
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
